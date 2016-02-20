967a2ba279cc7d7df57f1cce2b9fdc61f3500c7c
Mohamed Sharif <mohame@gmail.com>
2016-02-20 13:07:26 -0600

    second commit

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -10,6 +10,35 @@ namespace Cheers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter your name");
+            string name = Console.ReadLine();
+            name =  name.ToUpper();
+            Console.WriteLine("Your Name is  " + name.Length + " Character Long");
+            Char [] Letters = new[] { 'H', 'A', 'L', 'F', 'N', 'O', 'R', 'S', 'S', 'E', 'M', 'I', 'X' };
+           
+            Char[] NewNames = name.ToCharArray();
+
+
+            foreach (var temp in NewNames)
+            {   
+                if (Letters.Contains(temp))
+                {   
+                    Console.WriteLine("Give me an  " + temp);
+                }
+                else
+                {
+                    Console.WriteLine("Give me a   " + temp + " ");
+
+                }
+
+               
+            }
+            Console.WriteLine("Cheers Your Name is  " + name.ToUpper());
+        }
+
+        private static char Contains(int v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
