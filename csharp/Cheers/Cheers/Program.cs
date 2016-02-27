aec586eb5187b5b517ffa7a2422a96f4ae4dfb84
Mohamed Sharif <mohame@gmail.com>
2016-02-27 13:54:50 -0600

    done with this.

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -12,17 +12,20 @@ namespace Cheers
         {
             Console.WriteLine("Please Enter your name");
             string name = Console.ReadLine();
-            name =  name.ToUpper();
+            Console.WriteLine("Please inter your birthday. e.x.10/10");
+            DateTime BirthDate = DateTime.Parse(Console.ReadLine());
+
+            name = name.ToUpper();
             Console.WriteLine("Your Name is  " + name.Length + " Character Long");
-            Char [] Letters = new[] { 'H', 'A', 'L', 'F', 'N', 'O', 'R', 'S', 'S', 'E', 'M', 'I', 'X' };
-           
+            Char[] Letters = new[] { 'H', 'A', 'L', 'F', 'N', 'O', 'R', 'S', 'S', 'E', 'M', 'I', 'X' };
+
             Char[] NewNames = name.ToCharArray();
 
 
             foreach (var temp in NewNames)
-            {   
+            {
                 if (Letters.Contains(temp))
-                {   
+                {
                     Console.WriteLine("Give me an  " + temp);
                 }
                 else
@@ -31,14 +34,28 @@ namespace Cheers
 
                 }
 
-               
-            }
-            Console.WriteLine("Cheers Your Name is  " + name.ToUpper());
-        }
 
-        private static char Contains(int v)
-        {
-            throw new NotImplementedException();
+            }
+
+            TimeSpan DayLeft = BirthDate.Subtract(DateTime.Now);
+            DateTime CurrentDate = DateTime.Now;
+            
+
+            if (DayLeft.TotalDays < -1)
+            {
+                Console.WriteLine("Your Birthay is {0:0} days left", DayLeft.TotalDays+365 );
+            }
+            else if (DayLeft.TotalDays >= -1 && DayLeft.TotalDays < 0)
+            {
+                Console.WriteLine("Happy birthday");
+            }        
+            else
+            {
+                Console.WriteLine("Yur Birthday is {0:0} Days Left ",DayLeft.TotalDays);
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
